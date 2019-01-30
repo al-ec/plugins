16bad3cf6541fbcbaa23ea640d9914fcbd707916
Alec Armbruster <alecsamuelarmbruster@gmail.com>
2019-01-30 13:03:14 -0800

    Update UnboundBL.sh

--- dns/unboundbl/src/opnsense/scripts/OPNsense/Unboundbl/UnboundBL.sh
+++ dns/unboundbl/src/opnsense/scripts/OPNsense/Unboundbl/UnboundBL.sh
@@ -2,7 +2,8 @@
 # UnboundBL, UnboundBL.sh
 
 # include config-generated blacklist/whitelist, commas replaced with spaces
-source /usr/local/opnsense/scripts/OPNsense/Unboundbl/data.sh
+# I learned the hard way, 'source' is the wrong command! ):
+. /usr/local/opnsense/scripts/OPNsense/Unboundbl/data.sh
 
 # prep temp storage and conf file
 touch /tmp/hosts.working
@@ -14,7 +15,9 @@ done
 
 # sort all the lists and remove any whitelist items!
 awk -v whitelist="$whitelist" '$1 ~ /^127\.|^0\./ && $2 !~ whitelist {gsub("\r",""); print tolower($2)}' /tmp/hosts.working | sort | uniq | \
+# generate that unbound file, yo
 awk '{printf "server:\n", $1; printf "local-data: \"%s A 0.0.0.0\"\n", $1}' > /var/unbound/UnboundBL.conf
 
 # clear the temp storage!
+# GET IT OUT OF HERE
 rm /tmp/hosts.working
